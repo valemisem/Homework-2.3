48046e143583e8c6095e4c1c94920fd7c1debd20
valemisem <valentina.misem@gmail.com>
2023-10-27 20:55:12 +0200

    [fff]

---
+++ collections.js
@@ -0,0 +1,10 @@
+let block = ['querty', 'taco', 'yle', 'car', 'map']
+console.log (block.length)
+console.log(block[1])
+
+console.log(block.includes('taco', 2)) // true or false, false STARTING FROM 2ND ELEMENT
+
+// RETURN INDEX NUMBER OF AN ELEMENT
+console.log(block.indexOf('yle'))
+
+// add elements to the end of array
