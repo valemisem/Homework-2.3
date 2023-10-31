689d7f925df011465b4773e67b4017fd0f1231df
valemisem <valentina.misem@gmail.com>
2023-10-31 19:51:28 +0100

    1

--- collections.js
+++ collections.js
@@ -14,3 +14,24 @@ console.log(block) // output - array
 console.log(block.pop('login', 'bin')) // delete last element
 console.log(block)
 
+// array MATRIX - двухмерный массив
+let blockDeep = [
+    ['querty', 'taco', 'yle'],
+    ['car', 'map']
+]
+console.log(blockDeep[0][1])
+
+
+// map
+let mapSample = new Map()
+mapSample.set(['1', '2', '3'], 'first value')
+mapSample.set(Date.now(), 'sec value')
+console.log(mapSample)
+
+console.log(mapSample.get(Date.now()))
+
+// set
+let sampleTest = new Set()
+sampleTest.add('1')
+console.log(sampleTest)
+sampleTest.has ('1')
